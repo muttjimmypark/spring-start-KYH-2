179355fb1f8b7f6fec2b0144baede2e71e63c44b
muttjimmypark <muttpark57@gmail.com>
2022-10-11 16:22:29 +0900

    new

---
+++ src/main/java/hellogroup/hellospringartiname/HelloSpringArtinameApplication.java
@@ -0,0 +1,13 @@
+package hellogroup.hellospringartiname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringArtinameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringArtinameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellogroup/hellospringartiname/HelloSpringArtinameApplicationTests.java
@@ -0,0 +1,13 @@
+package hellogroup.hellospringartiname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringArtinameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
