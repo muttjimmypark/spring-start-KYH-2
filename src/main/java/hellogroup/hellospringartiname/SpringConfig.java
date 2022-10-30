3d0b3db62027136b83e2ffbc9010fad38821ecc0
muttjimmypark <muttpark57@gmail.com>
2022-10-31 06:21:52 +0900

    web mvc 개발

--- src/main/java/hellogroup/hellospringartiname/SpringConfig.java
+++ src/main/java/hellogroup/hellospringartiname/SpringConfig.java
@@ -9,6 +9,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    /**
+     * 컨트롤러-서비스-리포지토리로 정형화된 프로젝트는 컴포넌트 스캔 사용
+     *
+     * 정형화되지 않은 경우, (지금처럼) 구현클래스의 변경여지가 있는경우 config 작성
+     */
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
